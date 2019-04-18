1050d1cabdf8b65e212efe49520704b356fbef5b
Armando Bracho <ArmandoBracho@eurocarparks.com>
2019-04-18 15:17:52 +0100

    Added new Net standard class library

---
+++ src/NLog.Raygun.AspNetCore/RayGunTarget.cs
@@ -0,0 +1,75 @@
+﻿using Mindscape.Raygun4Net.AspNetCore;
+using NLog.Config;
+using NLog.Targets;
+using System;
+
+namespace NLog.Raygun.AspNetCore
+{
+    [Target("RayGunAspNetCore")]
+    public class RayGunTarget : TargetWithLayout
+    {
+        [RequiredParameter]
+        public string ApiKey { get; set; }
+
+        [RequiredParameter]
+        public string Tags { get; set; }
+
+        [RequiredParameter]
+        public string IgnoreFormFieldNames { get; set; }
+
+        [RequiredParameter]
+        public string IgnoreCookieNames { get; set; }
+
+        [RequiredParameter]
+        public string IgnoreServerVariableNames { get; set; }
+
+        [RequiredParameter]
+        public string IgnoreHeaderNames { get; set; }
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            var logMessage = Layout.Render(logEvent);
+
+            var exception = new RaygunException(logMessage, logEvent.Exception);
+
+            var client = CreateRaygunClient();
+
+            SendMessage(client, exception);
+        }
+
+        private RaygunClient CreateRaygunClient()
+        {
+            var client = new RaygunClient(ApiKey);
+
+            client.IgnoreFormFieldNames(SplitValues(IgnoreFormFieldNames));
+            client.IgnoreCookieNames(SplitValues(IgnoreCookieNames));
+            client.IgnoreHeaderNames(SplitValues(IgnoreHeaderNames));
+            client.IgnoreServerVariableNames(SplitValues(IgnoreServerVariableNames));
+
+            return client;
+        }
+
+        private void SendMessage(RaygunClient client, Exception exception)
+        {
+            if (!string.IsNullOrWhiteSpace(Tags))
+            {
+                var tags = Tags.Split(',');
+                client.SendInBackground(exception, tags);
+            }
+            else
+            {
+                client.SendInBackground(exception);
+            }
+        }
+
+        private string[] SplitValues(string input)
+        {
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                return input.Split(',');
+            }
+
+            return new[] { "" };
+        }
+    }
+}
---
+++ src/NLog.Raygun.AspNetCore/RaygunException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NLog.Raygun.AspNetCore
+{
+    public class RaygunException : Exception
+    {
+        public RaygunException(string message)
+            : base(message)
+        {
+
+        }
+
+        public RaygunException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
